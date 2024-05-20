e7132a110e115ce4f102bee157e82f30378cf22c
bao <mimun0407@gmail.com>
2024-05-20 08:44:21 +0700

    xem chi tiets cart

--- src/main/java/com/example/shopeefood/controller/CartController.java
+++ src/main/java/com/example/shopeefood/controller/CartController.java
@@ -1,33 +1,45 @@
 package com.example.shopeefood.controller;
 
 import com.example.shopeefood.model.Cart;
-import com.example.shopeefood.repository.ICartRepository;
+import com.example.shopeefood.model.User;
+import com.example.shopeefood.service.cart.ICartService;
+import com.example.shopeefood.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/api/carts")
 public class CartController {
     @Autowired
-    ICartRepository cartRepository;
-    @PutMapping("/{id}")
-    public ResponseEntity<Cart> updateCart(@PathVariable long id, @RequestBody Cart cart) {
-        cart.setId(id);
-        Cart cart1=cartRepository.save(cart);
-        return new ResponseEntity<Cart>(cart1, HttpStatus.CREATED);
-    }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Cart> deleteCart(@PathVariable long id) {
-        cartRepository.deleteById(id);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+    private IUserService iUserService;
+    @Autowired
+    private ICartService iCartService;
     @GetMapping
-    public ResponseEntity<List<Cart>> getCarts() {
-        List<Cart> carts=cartRepository.findAll();
-        return new ResponseEntity<>(carts, HttpStatus.OK);
+    public ResponseEntity<List<Cart>> getAllCarts() {
+        List<Cart> cartList = (List<Cart>) iCartService.findAll();
+        if (cartList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<>(cartList, HttpStatus.OK);
+        }
+    }
+    //    @PostMapping
+//    public ResponseEntity<Cart> addCart(@RequestBody Cart cart) {
+//        return new ResponseEntity<>(iCartService.save(cart), HttpStatus.CREATED);
+//    }
+    @GetMapping("/{idUser}")
+    public ResponseEntity<List<Cart>> findCartByIdUser(@PathVariable long idUser) {
+        Optional<User> user = iUserService.findById(idUser);
+        List<Cart> cartList = (List<Cart>) iCartService.findCartByIdUser(user.get());
+        if (cartList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<>(cartList, HttpStatus.OK);
+        }
     }
 }
--- src/main/java/com/example/shopeefood/controller/CategoryController.java
+++ src/main/java/com/example/shopeefood/controller/CategoryController.java
@@ -7,9 +7,7 @@ import com.example.shopeefood.service.city.ICityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +24,5 @@ public class CategoryController {
         }
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
+
 }
